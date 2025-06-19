28587c2b63d91bddf4cbaed2822b71715c020c23
facundo <facundobecker000@gmail.com>
2025-06-19 10:12:31 -0300

    Update 01-create-users.sql
    Incluir esquma de login a la tabla users.

--- backend/db/init.d/01-create-users.sql
+++ backend/db/init.d/01-create-users.sql
@@ -1,1 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+  id               SERIAL PRIMARY KEY,
+  usuario          VARCHAR(50) UNIQUE NOT NULL,
+  contraseña       VARCHAR(255) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  mail             VARCHAR(100) UNIQUE NOT NULL,
+  telefono         VARCHAR(20)
+);
 

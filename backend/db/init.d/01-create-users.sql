1b9fad1acad72a3c9672293bfcb032f223a65cc4
Ian Chen <chenian317@gmail.com>
2025-06-21 21:06:27 -0300

    cambiar nombre y orden de columnas

--- backend/db/init.d/01-create-users.sql
+++ backend/db/init.d/01-create-users.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
-  id               SERIAL PRIMARY KEY,
-  usuario          VARCHAR(50) UNIQUE NOT NULL,
-  contraseña       VARCHAR(255) NOT NULL,
-  fecha_nacimiento DATE NOT NULL,
-  mail             VARCHAR(100) UNIQUE NOT NULL,
-  telefono         VARCHAR(20)
+  id                INT PRIMARY KEY,
+  username          VARCHAR(50) UNIQUE NOT NULL,
+  email             VARCHAR(100) UNIQUE NOT NULL,
+  phone             VARCHAR(20)
+  hashed_password   VARCHAR(255) NOT NULL,
+  register_date     DATE NOT NULL,
 );
 

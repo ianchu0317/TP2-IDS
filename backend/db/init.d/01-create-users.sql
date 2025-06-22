e4286c18ea09d0bfc65574e4c52e7f19c8ba6f6f
Ian Chen <chenian317@gmail.com>
2025-06-21 21:23:16 -0300

    configurar usuarios y formato de tablas

--- backend/db/init.d/01-create-users.sql
+++ backend/db/init.d/01-create-users.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS users (
   id                INT PRIMARY KEY,
   username          VARCHAR(50) UNIQUE NOT NULL,
   email             VARCHAR(100) UNIQUE NOT NULL,
-  phone             VARCHAR(20)
+  phone             VARCHAR(20),
   hashed_password   VARCHAR(255) NOT NULL,
-  register_date     DATE NOT NULL,
+  register_date     DATE NOT NULL
 );
 

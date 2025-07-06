13926720e7e84f3ba7c3268d559eae491ffcfb81
facundo <facundobecker000@gmail.com>
2025-07-06 18:44:09 -0300

    Update 02.create-phobias.sql
    Creamos la tabla con sus respectivos valores de cada fobia.

---
+++ backend/db/init.d/02.create-phobias.sql
@@ -0,0 +1,9 @@
+-- 02-create-phobias.sql
+CREATE TABLE IF NOT EXISTS phobias (
+  id           SERIAL PRIMARY KEY,
+  phobia_name  VARCHAR(100) NOT NULL,
+  description  TEXT,
+  creator_id   INT NOT NULL,
+  likes        INT DEFAULT 0,
+  date         DATE NOT NULL
+);

ba8f7bc6de62ff7ae13156f873fc7f280d87c5b1
facundo <facundobecker000@gmail.com>
2025-07-06 18:47:46 -0300

    Update 03-create-comments.sql
    creamos la tabla comments con sus respectivos valores

--- backend/db/init.d/03-create-comments.sql
+++ backend/db/init.d/03-create-comments.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS comments (
+  id          SERIAL PRIMARY KEY,
+  comment     VARCHAR(255) NOT NULL,
+  creator_id  INT NOT NULL,
+  phobia_id   INT NOT NULL,
+  likes       INT DEFAULT 0,
+  date        DATE NOT NULL
+);

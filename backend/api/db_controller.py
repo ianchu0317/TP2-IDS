4fbbf40dab0c6e0fee0eb74c76fb9b48edaabf21
Ian Chen <chenian317@gmail.com>
2025-07-07 11:29:26 -0300

    feat: buscar info de usuario en db

--- backend/api/db_controller.py
+++ backend/api/db_controller.py
@@ -206,3 +206,25 @@ async def get_comments(phobia_id: int):
                 ))
     aconn.close()
     return comments
+
+async def get_user_info(user_id: int) -> UserInDB | None:
+    aconn = await psycopg.AsyncConnection.connect(
+        "host=db dbname=tp2 user=fobias password=fobias")
+    
+    async with aconn:
+        async with aconn.cursor() as acur:
+            await acur.execute(
+                "SELECT * FROM users " 
+                f"WHERE id={user_id}")
+            
+            user_db_data = await acur.fetchone() 
+    aconn.close()
+    
+    return UserInDB(
+        id=user_db_data[0],
+        username=user_db_data[1],
+        email=user_db_data[2],
+        phone=user_db_data[3],
+        hashed_password=user_db_data[4],
+        date=user_db_data[5]
+    )

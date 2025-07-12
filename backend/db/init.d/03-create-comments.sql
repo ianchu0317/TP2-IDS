3c87d70c855bbc538e42ca7836639824aaddf818
Ian Chen <chenian317@gmail.com>
2025-07-12 18:05:06 -0300

    feat: expandir tamaño de comentarios

--- backend/db/init.d/03-create-comments.sql
+++ backend/db/init.d/03-create-comments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS comments (
   id          SERIAL PRIMARY KEY,
-  comment     VARCHAR(255) NOT NULL,
+  comment     VARCHAR(1024) NOT NULL,
   creator_id  INT NOT NULL,
   phobia_id   INT NOT NULL,
   date        DATE NOT NULL

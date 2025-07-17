cc25ec5ab57a9a6b8a1b98b47d80b03df9e36543
facundo <facundobecker000@gmail.com>
2025-07-17 19:52:02 -0300

    add foreign key in comments for users and phobias. Add new date, equal to current date.

--- backend/db/init.d/03-create-comments.sql
+++ backend/db/init.d/03-create-comments.sql
@@ -3,5 +3,18 @@ CREATE TABLE IF NOT EXISTS comments (
   comment     VARCHAR(1024) NOT NULL,
   creator_id  INT NOT NULL,
   phobia_id   INT NOT NULL,
-  date        DATE NOT NULL
+  date        DATE NOT NULL CURRENT DATE,
+
+  -- Foreign key a users
+  CONSTRAINT fk_comment_creator
+  FOREIGN KEY (creator_id)
+  REFERENCES users(id)
+
+  -- Foreign key a phobias
+  CONSTRAINT fk_comment_phobia
+  FOREIGN KEY (phobia_id)
+  REFERENCES phobias(id) 
+
+
+  
 );

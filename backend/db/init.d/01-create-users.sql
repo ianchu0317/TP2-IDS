a001370484edb7f3d68cbc6e629fa4e68b0e4eaf
Ian Chen <71509578+ianchu0317@users.noreply.github.com>
2025-07-18 21:25:37 -0300

    Revert "Dev"

--- backend/db/init.d/01-create-users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  id                SERIAL PRIMARY KEY,
-  username          VARCHAR(50) UNIQUE NOT NULL,
-  email             VARCHAR(100) UNIQUE NOT NULL,
-  phone             VARCHAR(20),
-  hashed_password   VARCHAR(255) NOT NULL,
-  register_date     DATE NOT NULL
-);
-
--- backend/db/init.d/04-create-ai-user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (username, email, phone, hashed_password, register_date)
-VALUES ('skibidiGPT', 'skibidi@gpt.com', 123, 'NULL', CURRENT_TIMESTAMP);

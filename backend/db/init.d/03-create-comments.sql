edec0fcf8f861ea7a8a827fc71c2a5df7f902e39
Lia Laos <ljhong@fi.uba.ar>
2025-07-22 21:41:56 +0000

    fix: correct SQL syntax error in comments table creation

--- backend/db/init.d/03-create-comments.sql
+++ backend/db/init.d/03-create-comments.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS comments (
   comment     VARCHAR(1024) NOT NULL,
   creator_id  INT NOT NULL,
   phobia_id   INT NOT NULL,
-  date        DATE NOT NULL CURRENT DATE,
+  date        DATE NOT NULL DEFAULT CURRENT_DATE,
 
   -- Foreign key a users
   CONSTRAINT fk_comment_creator
   FOREIGN KEY (creator_id)
-  REFERENCES users(id)
+  REFERENCES users(id),
 
   -- Foreign key a phobias
   CONSTRAINT fk_comment_phobia
